c9fdfe677f4cab2f3140076aa7202a39703156de
aryaziai <arya721@gmail.com>
2020-02-24 21:30:35 -0800

    change db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_05_235238) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.integer "user_id"
